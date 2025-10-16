4472c39ce4d2bc6fb45453be1eac390f4fb26154
Rami Alrwais <raalrwai@ucsc.edu>
2025-10-16 14:52:26 -0400

    RAG using Pinecone and OpenAI

--- jupyter/Nexus_text_processing/removeWidgets.py
+++ jupyter/Nexus_text_processing/removeWidgets.py
@@ -1,8 +1,19 @@
+# import nbformat
+
+# nb = nbformat.read("Nexus_Text_Processing_and_Generation_copy.ipynb", as_version=4)
+
+# if "widgets" in nb.metadata:
+#     del nb.metadata["widgets"]
+
+# nbformat.write(nb, "Nexus_Text_Processing_and_Generation_copy.ipynb")
 import nbformat
 
-nb = nbformat.read("Nexus_Text_Processing_and_Generation_copy.ipynb", as_version=4)
+with open("Nexus_Text_Processing_and_Generation_copy.ipynb", "r", encoding="utf-8") as f:
+    nb = nbformat.read(f, as_version=4)
 
-if "widgets" in nb.metadata:
-    del nb.metadata["widgets"]
+# This will raise errors if the notebook is malformed
+nbformat.validate(nb)
 
-nbformat.write(nb, "Nexus_Text_Processing_and_Generation_copy.ipynb")
+# Optionally write it back to fix minor issues
+with open("Nexus_Text_Processing_and_Generation_copy.ipynb", "w", encoding="utf-8") as f:
+    nbformat.write(nb, f)
